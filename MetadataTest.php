84e7f097d9625eb4720bd938434112d39802d7e6
Chris Harris <c.harris@communicationinnovations.com.au>
2016-10-31 17:33:43 +1000

    added some basic tests

---
+++ MetadataTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class MetadataTest extends TestCase
+{
+    public function testBasePageGood()
+    {
+
+        $this->visit('odata.svc')
+        ->see('<service')
+        ->see('xmlns:atom="http://www.w3.org/2005/Atom"')
+        ->see('xmlns:app="http://www.w3.org/2007/app')
+        ->see('xmlns="http://www.w3.org/2007/app"')
+        ->see('xml:base="')
+        ->see('</service>')
+        ->see('<workspace>')
+        ->see('</workspace>')
+        ->see('<atom:title>Default</atom:title>')
+        ->see('<collection href="customer">')
+        ->see('<collection href="photo">')
+        ->see('<collection href="product">')
+        ->see('<collection href="staff">')
+        ->dontSee("Whoops");
+    }
+}
