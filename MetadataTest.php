f67042b1b0a890e382405d16ab2d2be6d932aa5b
Chris Harris <c.harris@communicationinnovations.com.au>
2016-10-31 18:47:47 +1000

    basic metadata teests

--- MetadataTest.php
+++ MetadataTest.php
@@ -22,4 +22,29 @@ class MetadataTest extends TestCase
         ->see('<collection href="staff">')
         ->dontSee("Whoops");
     }
+
+    public function testMetadataPageGood()
+    {
+        $this->visit('/odata.svc/$metadata')
+        ->see('<edmx:Edmx')
+        ->see('<edmx:DataServices')
+        ->see('<Schema Namespace=')
+        ->see('<EntityType Name="Staff">')
+        ->see('<EntityType Name="Customer">')
+        ->see('<EntityType Name="Photo" m:HasStream="true">')
+        ->see('<Association Name="Staff_Partner">')
+        ->see('<Association Name="Staff_Customers">')
+        ->see('<Association Name="Customer_Staff">')
+        ->see('<EntityContainer Name="Data" m:IsDefaultEntityContainer="true">')
+        ->see('<EntitySet Name="Staff" EntityType="Data.Staff"/>')
+        ->see('<EntitySet Name="Customer" EntityType="Data.Customer"/>')
+        ->see('<EntitySet Name="Photo" EntityType="Data.Photo"/>')
+        ->see('<AssociationSet Name="Staff_Partner_Staff" Association="Data.Staff_Partner">')
+        ->see('<AssociationSet Name="Staff_Customers_Customer" Association="Data.Staff_Customers">')
+        ->see('<AssociationSet Name="Customer_Staff_Staff" Association="Data.Customer_Staff">')
+        ->see('</Schema>')
+        ->see('</edmx:DataServices>')
+        ->dontSee("Whoops");
+
+    }
 }
