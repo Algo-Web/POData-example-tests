6ec050b495abaebaf3e4d9f20fe802686d75fc12
Chris Harris <c.harris@communicationinnovations.com.au>
2016-11-02 15:22:03 +1000

    updated tests

--- CustomerCollectionTest.php
+++ CustomerCollectionTest.php
@@ -23,4 +23,26 @@ class CustomerCollectionTest extends TestCase
 
         ->dontSee("Whoops");
     }
+
+    public function testSingleCollection()
+   {
+        $this->visit('odata.svc/customer(1)')
+        ->see('<?xml version="1.0" encoding="UTF-8" standalone="yes"?>')
+        ->see('<entry xml:base="http://')
+        ->see('/customer(id=1)</id>')
+        ->see('<title type="text">customer</title>')
+        ->see('<author>')
+        ->see('</author>')
+        ->see('<link rel="http://schemas.microsoft.com/ado/2007/08/dataservices/related/staff" type="application/atom+xml;type=entry" title="staff" href="customer(id=1)/staff"/>')
+        ->see('<category term="Data.customer" scheme="http://schemas.microsoft.com/ado/2007/08/dataservices/scheme"/>')
+        ->see('<content type="application/xml"')
+        ->see('<m:properties>')
+        ->see('<d:id m:type="Edm.Int32">1</d:id>')
+        ->see('<d:name m:type="Edm.String">Bilbo Baggins</d:name>')
+        ->see('</m:properties>')
+        ->see('</content>')
+        ->see('</entry>')
+        ->dontSee("Whoops");
+   }
+
 }
